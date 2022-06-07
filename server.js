d0007d97f9a6610f9e2211f4d9102439b397f493
Luciana Chamorro <lucianachamorro87@gmail.com>
2022-06-07 13:38:25 -0300

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cors= require('cors');
 const path = require('path');
 
-const contactRoute = require('./route/contact.js');
+const contactRoute = require('./route/contactRoute.js');
 const app = express();
 
 //app va a usar el middleware cors
@@ -13,6 +13,13 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/', contactRoute);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    );
+  }
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`Server running on port ${port}`));
